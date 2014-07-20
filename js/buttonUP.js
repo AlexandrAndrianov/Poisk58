30ca9d987ba2e276e34547f4f37e9189cc60b0a2
AlexandrAndrianov <andatrGitHub@yandex.ru>
2014-07-20 14:34:07 +0500

    Сделал скрипт buttonUP.js для кнопки "Вверх" добавил скрипт в putevoditel.html

---
+++ js/buttonUP.js
@@ -0,0 +1,47 @@
+
+
+
+var displ = window.innerHeight;
+displ = displ - 100;
+
+var btn = document.getElementById('btnUp');
+btn.style.top = displ + 'px';
+btn.addEventListener('click', up, false);
+
+function up()
+{
+	var pixel = 18;
+	var cutoff = 100;
+	var speed = 15;
+	
+	var scrollCur = window.pageYOffset 
+					|| document.documentElement.scrollTop 
+					|| document.body.scrollTop;
+					
+	var animate = setInterval(
+		function()
+		{	
+			scrollCur = scrollCur - pixel;
+			window.scrollBy(0, -pixel);
+			if(scrollCur < 0) clearInterval(animate);
+			//if(scrollCur < cutoff) pixel = 4;
+		}, speed);
+}
+
+
+window.addEventListener('scroll',btnUp,false);
+function btnUp()
+{
+	var cutoffbtn = 280;
+	var scrollCur = window.pageYOffset 
+					|| document.documentElement.scrollTop 
+					|| document.body.scrollTop;
+	
+	var scrollDown = document.documentElement.scrollHeight -
+		scrollCur -
+		document.documentElement.clientHeight;
+	
+	if( scrollCur > cutoffbtn) btn.style.visibility = 'visible';
+	if( scrollCur < cutoffbtn) btn.style.visibility = 'hidden';
+	console.log(scrollCur);
+}
