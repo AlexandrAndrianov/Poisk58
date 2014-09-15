be59081cfa2c419cb0f7f90670e8008b26b166fe
AlexandrAndrianov <andatrGitHub@yandex.ru>
2014-09-15 12:41:13 +0400

    Сделал индивидуальный ID  для каждой галлереи в компоненте list

--- poisk58bitrix/components/list/ResponsiveImageGallery/js/gallery1.js
+++ poisk58bitrix/components/list/ResponsiveImageGallery/js/gallery1.js
@@ -1,73 +1,63 @@
-function toglDwUp(el){
+$(function() {
 
-var HIDDEN = 'rgb(255, 255, 255)';
-var VISIBLE = 'rgb(51, 51, 51)';
+	// ======================= imagesLoaded Plugin ===============================
+	// https://github.com/desandro/imagesloaded
 
-var flag = $($(el).children()[0]).css('border-bottom-color');
+	// $('#my-container').imagesLoaded(myFunction)
+	// execute a callback when all images have loaded.
+	// needed because .load() doesn't work on cached images
 
-if(flag === VISIBLE){
-	var DwUp = $(el).children()[0];
-	$(DwUp).css('border-bottom-color', HIDDEN);
-	$(DwUp).css('border-left-color', HIDDEN);
+	// callback function gets image collection as argument
+	//  this is the container
+
+	// original: mit license. paul irish. 2010.
+	// contributors: Oren Solomianik, David DeSandro, Yiannis Chatzikonstantinou
 	
-	$(DwUp).css('border-top-color', VISIBLE);
-	$(DwUp).css('border-right-color', VISIBLE);
-}else{
-	var DwUp = $(el).children()[0];
-	$(DwUp).css('border-bottom-color', VISIBLE);
-	$(DwUp).css('border-left-color', VISIBLE);
-	
-	$(DwUp).css('border-top-color', HIDDEN);
-	$(DwUp).css('border-right-color', HIDDEN);
-}
-}
 
-function galeryLoad(el){
-	
 	$.fn.imagesLoaded 		= function( callback ) {
-		var $images = this.find('img'),
-			len 	= $images.length,
-			_this 	= this,
-			blank 	= 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+	var $images = this.find('img'),
+		len 	= $images.length,
+		_this 	= this,
+		blank 	= 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
 
-		function triggerCallback() {
-			callback.call( _this, $images );
+	function triggerCallback() {
+		callback.call( _this, $images );
+	}
+
+	function imgLoaded() {
+		if ( --len <= 0 && this.src !== blank ){
+			setTimeout( triggerCallback );
+			$images.off( 'load error', imgLoaded );
 		}
+	}
 
-		function imgLoaded() {
-			if ( --len <= 0 && this.src !== blank ){
-				setTimeout( triggerCallback );
-				$images.off( 'load error', imgLoaded );
-			}
+	if ( !len ) {
+		triggerCallback();
+	}
+
+	$images.on( 'load error',  imgLoaded ).each( function() {
+		// cached images don't fire load sometimes, so we reset src.
+		if (this.complete || this.complete === undefined){
+			var src = this.src;
+			// webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
+			// data uri bypasses webkit log warning (thx doug jones)
+			this.src = blank;
+			this.src = src;
 		}
-
-		if ( !len ) {
-			triggerCallback();
-		}
-
-		$images.on( 'load error',  imgLoaded ).each( function() {
-			// cached images don't fire load sometimes, so we reset src.
-			if (this.complete || this.complete === undefined){
-				var src = this.src;
-				// webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
-				// data uri bypasses webkit log warning (thx doug jones)
-				this.src = blank;
-				this.src = src;
-			}
-		});
+	});
 
 	return this;
 	};
-	
+
 	// gallery container
-	var $rgGallery			= $('#'+el+'_galleryList'),
+	var $rgGallery			= $('#rg-gallery'),
 	// carousel container
 	$esCarousel			= $rgGallery.find('div.es-carousel-wrapper'),
-	
-	$items			= $rgGallery.find('i.mnlstel-ico.glyphicon.glyphicon-camera'),
+	// the carousel items
+	$items				= $esCarousel.find('ul > li'),
 	// total number of items
 	itemsCount			= $items.length;
-
+	
 	Gallery				= (function() {
 			// index of the current item
 		var current			= 0, 
@@ -75,36 +65,19 @@ function galeryLoad(el){
 			mode 			= 'carousel',
 			// control if one image is being loaded
 			anim			= false,
-			init			= function(e) {
+			init			= function() {
+				
 				// (not necessary) preloading the images here...
-				$items.add('<img src="/bitrix/components/andatr/list/images/ajax-loader.gif"/><img src="/bitrix/components/andatr/list/images/black.png"/>').imagesLoaded( function() {
+				$items.add('<img src="/bitrix/components/andatr/fotogaleryLait/images/ajax-loader.gif"/><img src="/bitrix/components/andatr/fotogaleryLait/images/black.png"/>').imagesLoaded( function() {
 					// add options
 					//_addViewModes();
 					
 					// add large image wrapper
 					_addImageWrapper();
 					
-					switch(e.target.nodeName){
-						case 'I':
-							// show first image
-							_showImage($(e.target));
-							break;
+					// show first image
+					_showImage( $items.eq( current ) );
 						
-						case 'TD':
-							var tr = $(e.target).parent();
-							var eltarget = tr.find('i.mnlstel-ico.glyphicon.glyphicon-camera');
-							_showImage(eltarget);
-							break;
-							
-						case 'SPAN':	
-							var tr = $(e.target).parent().parent();
-							var eltarget = tr.find('i.mnlstel-ico.glyphicon.glyphicon-camera');
-							_showImage(eltarget);
-							break;
-					}
-						
-					$rgGallery.unbind();		
-							
 				});
 				
 				// initialize the carousel
@@ -132,7 +105,7 @@ function galeryLoad(el){
 				// set elastislide's current to current
 				$esCarousel.elastislide( 'setCurrent', current );
 				
-				//ÑÐºÑ€Ñ‹Ð²Ð°ÐµÐ¼ Ð¿Ñ€ÐµÐ´Ð¿Ñ€Ð¾ÑÐ¼Ð¾Ñ‚Ñ€ Ñ„Ð¾Ñ‚Ð¾
+				//ñêðûâàåì ïðåäïðîñìîòð ôîòî
 				$esCarousel.hide();
 				
 			},
@@ -172,7 +145,7 @@ function galeryLoad(el){
 				// adds the structure for the large image and the navigation buttons (if total items > 1)
 				// also initializes the navigation events
 				
-				$('#'+el+'_img-wrapper-tmpl').tmpl( {itemsCount : itemsCount} ).prependTo( $rgGallery );
+				$('#img-wrapper-tmpl').tmpl( {itemsCount : itemsCount} ).prependTo( $rgGallery );
 			
 				if( itemsCount > 1 ) {
                                 
@@ -247,12 +220,12 @@ function galeryLoad(el){
 				$items.removeClass('selected');
 				$item.addClass('selected');
 					 
-				var $thumb		= $item,
+				var $thumb		= $item.find('img'),
 					largesrc	= $thumb.data('large'),
 					title		= $thumb.data('description');
-					count   = $thumb.data('count');
-		
-				$('#'+el+'_cntfoto').html(count+'/'+itemsCount);
+					count   = $thumb.attr('count');
+					
+				$('#cntfoto').html(count+'/'+itemsCount);
 					
 				//alert(title);
 				$('<img/>').load( function() {
@@ -287,8 +260,15 @@ function galeryLoad(el){
 			init 		: init,
 			addItems	: addItems
 		};
-
-	})();
 	
-	$rgGallery.bind('click', Gallery.init);	
-}
+	})();
+
+	Gallery.init();
+	
+	/*
+	Example to add more items to the gallery:
+	
+	var $new  = $('<li><a href="#"><img src="images/thumbs/1.jpg" data-large="images/1.jpg" alt="image01" data-description="From off a hill whose concave womb reworded" /></a></li>');
+	Gallery.addItems( $new );
+	*/
+});
--- poisk58bitrix/components/list/templates/.default/template.php
+++ poisk58bitrix/components/list/templates/.default/template.php
@@ -1,5 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();?>
 
+
 <?
 	if(!function_exists('translit'))
 	{
@@ -8,29 +9,93 @@
 	}
 	
 ?>
-
-	<?$cntItem = count($arResult); 
-		for($i = 1; $i <= $cntItem; $i++):?>
-			<?foreach($arResult[$i] as $title => $list):?>
-				<a href="javascript:void(0);" data-toggle="collapse" data-target="#<?=translit($title);?>">
-					<?=$title?>
-				</a>
-
-				<div id="<?=translit($title);?>" class="collapse ">
-					<?foreach($list as $ar => $elem ):?>
-						<a href="javascript:void(0);" data-toggle="collapse" data-target="#<?=translit($title).translit($ar);?>">
-							<?=$ar?>
+		<div id="fotogaleryList">
+			<?$cntItem = count($arResult); 
+				for($i = 1; $i <= $cntItem; $i++):?>
+					<?foreach($arResult[$i] as $title => $list):?>
+						<a class="menu-list menu-list-title" href="javascript:void(0);" 
+							data-toggle="collapse" data-target="#<?=translit($title);?>" onclick="toglDwUp(this);">
+							<?=$title?>
+							<i></i>
 						</a>
 
-						<div id="<?=translit($title).translit($ar);?>" class="collapse ">
-							<?
-								$cntEl = count($elem);
-								for($e = 1; $e <= $cntEl; $e++):?>
-									<p><?=$elem[$e]?></p>
-							<?endfor?>
+						<div id="<?=translit($title);?>" class="collapse ">
+							<?foreach($list as $ar => $elem ):?>
+							
+									<noscript>
+										<style>
+											.es-carousel ul{
+												display:block;
+											}
+										</style>
+									</noscript>
+									
+									<script id="<?=translit($title).translit($ar);?>_img-wrapper-tmpl" type="text/x-jquery-tmpl">
+												<div class="rg-image-wrapper">
+													{{if itemsCount > 1}}                                    
+														<div class="rg-image-nav">
+															<span id="<?=translit($title).translit($ar);?>_cntfoto" 
+																class="cntfoto"></span>
+															<a id='prev' href="#" class="rg-image-nav-prev">Previous Image</a>
+															<a id='next' href="#" class="rg-image-nav-next">Next Image</a>
+														</div>
+													{{/if}}
+													<div class="rg-image"></div>
+													<div class="rg-loading"></div>
+													<div class="rg-caption-wrapper">
+														<div class="rg-caption text_white" style="display:none;">
+															<p></p>
+														</div>
+													</div>
+												</div>	
+									</script>	
+							
+								<a class="menu-list menu-list-inside" href="javascript:void(0);" 
+									data-toggle="collapse" 
+									data-target="#<?=translit($title).translit($ar);?>" onclick="toglDwUp(this);">
+									<?=$ar?>
+									<i></i>
+								</a>
+							
+								<div id="<?=translit($title).translit($ar);?>" class="collapse ">
+									<div id="<?=translit($title).translit($ar);?>_galleryList" class="rg-gallery">
+										<table class="mnlst-table">
+												<col width="8%">
+												<col width="80%">
+												<col width="12%">
+												<?
+													$cntEl = 1;
+													while(!empty($elem[$cntEl])):?>
+													<?
+														preg_match('|(.+)\((.+)?\)(.+$)|', $elem[$cntEl], $mat)
+													?>	
+															<tr class="mnlst-table-row">
+															 <td class="mnlst-cntr">
+																<i  data-large="<?=$elem['F_'.$cntEl]?>"
+																	data-description="<?=$mat[1]?>"
+																	data-count ="<?=$cntEl?>"
+																	class="mnlstel-ico glyphicon glyphicon-camera"></i>
+															 </td>
+															 <td class="mnlst-descr">
+																<?=$mat[1]?><br/>
+																<span style="color:#ccc;"><?=$mat[2]?></span>
+															 </td>
+															 <td>
+																<?=$mat[3]?>
+															 </td>
+															</tr>
+												<?$cntEl++;
+													endwhile?>		
+										</table>
+										<script type="text/javascript" src="<?=$componentPath?>/ResponsiveImageGallery/js/jquery.tmpl.min.js"></script>
+										<script type="text/javascript" src="<?=$componentPath?>/ResponsiveImageGallery/js/jquery.elastislide.js"></script>
+										<script>
+											galeryLoad('<?=translit($title).translit($ar);?>');
+										</script>
+									</div><!-- rg-gallery -->
+								</div>
+							<?endforeach?>	
 						</div>
-					<?endforeach?>	
-				</div>
-			<?endforeach?>
-		<?endfor?>
-	
+					<?endforeach?>
+				<?endfor?>
+		</div><!-- fotogalery --> 
